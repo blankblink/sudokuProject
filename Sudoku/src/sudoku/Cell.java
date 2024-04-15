62824d9a7e48a57e6210f47b27f34944e2591863
yong0268 <yong0268@e.ntu.edu.sg>
2024-04-15 20:56:25 +0800

    Updated cell background colors

--- Sudoku/src/sudoku/Cell.java
+++ Sudoku/src/sudoku/Cell.java
@@ -12,9 +12,9 @@ public class Cell extends JTextField {
 
    // Define named constants for JTextField's colors and fonts
    //  to be chosen based on CellStatus
-   public static final Color BG_GIVEN = new Color(240, 240, 240); // RGB
-   public static final Color FG_GIVEN = Color.BLACK;
-   public static final Color FG_NOT_GIVEN = Color.GRAY;
+   public static final Color BG_GIVEN = new Color(0, 0, 0); // RGB 51-204-255 240
+   public static final Color FG_GIVEN = Color.WHITE;
+   public static final Color FG_NOT_GIVEN = Color.BLACK;
    public static final Color BG_TO_GUESS  = Color.YELLOW;
    public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
    public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
